3fb77c0b0e038b38450399b4ab0d0a5cad5d1e79
Belal Shehab <ma43222@gmail.com>
2021-06-01 03:08:30 +0200

    added some docs to the do_step and winner functions

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ def do_step(current_state, pocket_number, is_stealing=False):
     """
     Aya
     move stones from pocket_number to the next pockets handling:
-     1. the player mankala.
+     1. the player mancala.
      2. handles if stealing mode is on.
      3. set the next player.
+     4. check if all 6 pockets of a user is empty then empty the other user pockets and fill his mancala with it.
 
     :param is_stealing:
     :param pocket_number: from 0 to 5
     :param current_state: {
         "player": 0,
-        "mankala_state":[4, 4, 0, 2, 5, 0]
+        "mancala_state":[4, 4, 0, 2, 5, 0]
     }
     :return: new_state: {
         "player": 1,
-        "mankala_state":[0, 5, 1, 2, 5, 0]
+        "mancala_state":[0, 5, 1, 2, 5, 0]
     }
     """
     new_state = {
         "player": 1,
-        "mankala_state": [0, 5, 1, 2, 5, 0]
+        "mancala_state": [0, 5, 1, 2, 5, 0]
     }
     return new_state
 
@@ -32,7 +33,7 @@ def calculate_tree(current_state, depth):
         "option": 1,
         "player": 0,
         "score": 2,
-        "mankala_state":[2, 4, 0, 2, 5, 0],
+        "mancala_state":[2, 4, 0, 2, 5, 0],
         "steps": []
     }
     :param depth: number of steps to be planned
@@ -40,12 +41,12 @@ def calculate_tree(current_state, depth):
         "option": 1,
         "player": 0,
         "score": 2,
-        "mankala_state":[2, 4, 0, 2, 5, 0],
+        "mancala_state":[2, 4, 0, 2, 5, 0],
         "steps": [
             {
                 "player": 0,
                 "score": 2,
-                "mankala_state":[2, 4, 0, 2, 5, 0],
+                "mancala_state":[2, 4, 0, 2, 5, 0],
                 "steps": [
 
                 ]
@@ -53,7 +54,7 @@ def calculate_tree(current_state, depth):
             {
                 "player": 0,
                 "score": 2,
-                "mankala_state":[2, 4, 2, 2, 5, 0],
+                "mancala_state":[2, 4, 2, 2, 5, 0],
                 "steps": [
 
                 ]
@@ -67,10 +68,10 @@ def calculate_tree(current_state, depth):
         "mandala_state": [2, 0, 0, 2, 5, 0],
         "steps": []
     }
-    if current_state['mankala_state'][0] > 0:
+    if current_state['mancala_state'][0] > 0:
         new_state = do_step(current_state, 0, is_stealing=True)
         current_state['steps'].append(new_state)
-    if current_state['mankala_state'][1] > 0:
+    if current_state['mancala_state'][1] > 0:
         new_state = do_step(current_state, 1, is_stealing=True)
         current_state['steps'].append(new_state)
 
@@ -96,14 +97,14 @@ def think_of_move(current_state):
     :return: new_state {
         "player": 0,
         "score": 2,
-        "mankala_state":[4, 4, 0, 4, 4, 0],
+        "mancala_state":[4, 4, 0, 4, 4, 0],
         "steps": []
     }
     """
     new_state = {
         "player": 0,
         "score": 2,
-        "mankala_state": [4, 4, 0, 4, 4, 0],
+        "mancala_state": [4, 4, 0, 4, 4, 0],
         "steps": []
     }
     return new_state
@@ -112,8 +113,9 @@ def think_of_move(current_state):
 def winner(current_state):
     """
     Esraa
+    check if the game is finished by looking at each user six pockets (if all is empty then the game is finished)
     :param current_state:
-    :return: 0 for player0, 1 for player1, None
+    :return: 0 for player0, 1 for player1, None if the game is still running
     """
     return None
 
@@ -122,7 +124,7 @@ def main():
     game_state = {
         "player": input('enter 0 if you want to start'),
         "score": 2,
-        "mankala_state": [4, 4, 0, 4, 4, 0],
+        "mancala_state": [4, 4, 0, 4, 4, 0],
         "steps": []
     }
     is_stealing = input('enter 1 for stealing mode')
@@ -132,7 +134,7 @@ def main():
             # read user input:
             user_input = input('Enter number from 0 to 5')
             step_output = do_step(game_state, user_input, is_stealing)
-            game_state['mankala_state'] = step_output['mankala_state']
+            game_state['mancala_state'] = step_output['mancala_state']
             game_state['player'] = step_output['player']
         else:
             new_state = think_of_move(game_state)
