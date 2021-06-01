39083d4ae028d1df805ee7fe43fa5f2fe7d67a92
Belal Shehab <ma43222@gmail.com>
2021-06-01 22:57:33 +0200

    implemented calculate tree function

--- gui/mancala_gui.py
+++ gui/mancala_gui.py
@@ -13,7 +13,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setStyleSheet("color: white;""background-color: #222840;")
 
-
         ###########################  frames ###########################
 
         self.frame = QtWidgets.QFrame(self.centralwidget)
@@ -21,7 +20,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
-
 
         self.bts_frame = QtWidgets.QFrame(self.centralwidget)
         self.bts_frame.setGeometry(QtCore.QRect(90, 440, 761, 101))
@@ -35,14 +33,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         bt_font.setBold(True)
         bt_font.setWeight(75)
 
-
         lb_font = QtGui.QFont()
         lb_font.setPointSize(14)
         lb_font.setBold(True)
         lb_font.setWeight(75)
-
-
-
 
         ######################   Buttons  #########################
 
@@ -52,137 +46,121 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pocket1.setDefault(False)
         self.pocket1.setFlat(False)
         self.pocket1.setObjectName("pocket1")
-        
-        self.pocket1.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
 
-
+        self.pocket1.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket2 = QtWidgets.QPushButton(self.frame)
         self.pocket2.setGeometry(QtCore.QRect(220, 340, 60, 60))
         self.pocket2.setFont(bt_font)
         self.pocket2.setObjectName("pocket2")
-        self.pocket2.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
+        self.pocket2.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket3 = QtWidgets.QPushButton(self.frame)
         self.pocket3.setGeometry(QtCore.QRect(300, 340, 60, 60))
         self.pocket3.setObjectName("pocket3")
         self.pocket3.setFont(bt_font)
-        self.pocket3.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
+        self.pocket3.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket4 = QtWidgets.QPushButton(self.frame)
-        self.pocket4.setGeometry(QtCore.QRect(380, 340,  60, 60))
+        self.pocket4.setGeometry(QtCore.QRect(380, 340, 60, 60))
         self.pocket4.setObjectName("pocket4")
         self.pocket4.setFont(bt_font)
-        self.pocket4.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
+        self.pocket4.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
-        
         self.pocket5 = QtWidgets.QPushButton(self.frame)
-        self.pocket5.setGeometry(QtCore.QRect(470, 340,  60, 60))
+        self.pocket5.setGeometry(QtCore.QRect(470, 340, 60, 60))
         self.pocket5.setObjectName("pocket5")
         self.pocket5.setFont(bt_font)
-        self.pocket5.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
+        self.pocket5.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket6 = QtWidgets.QPushButton(self.frame)
-        self.pocket6.setGeometry(QtCore.QRect(570, 340,  60, 60))
+        self.pocket6.setGeometry(QtCore.QRect(570, 340, 60, 60))
         self.pocket6.setObjectName("pocket6")
         self.pocket6.setFont(bt_font)
-        self.pocket6.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
+        self.pocket6.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket7 = QtWidgets.QPushButton(self.frame)
-        self.pocket7.setGeometry(QtCore.QRect(570, 50,  60, 60))
+        self.pocket7.setGeometry(QtCore.QRect(570, 50, 60, 60))
         self.pocket7.setObjectName("pocket7")
         self.pocket7.setFont(bt_font)
-        self.pocket7.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
+        self.pocket7.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket8 = QtWidgets.QPushButton(self.frame)
-        self.pocket8.setGeometry(QtCore.QRect(470, 50,  60, 60))
+        self.pocket8.setGeometry(QtCore.QRect(470, 50, 60, 60))
         self.pocket8.setObjectName("pocket8")
         self.pocket8.setFont(bt_font)
-        self.pocket8.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
-
+        self.pocket8.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket9 = QtWidgets.QPushButton(self.frame)
         self.pocket9.setGeometry(QtCore.QRect(380, 50, 60, 60))
         self.pocket9.setObjectName("pocket9")
         self.pocket9.setFont(bt_font)
-        self.pocket9.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
+        self.pocket9.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
-
-        
         self.pocket10 = QtWidgets.QPushButton(self.frame)
         self.pocket10.setGeometry(QtCore.QRect(300, 50, 60, 60))
         self.pocket10.setObjectName("pocket10")
         self.pocket10.setFont(bt_font)
-        self.pocket10.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
+        self.pocket10.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
-        
         self.pocket11 = QtWidgets.QPushButton(self.frame)
         self.pocket11.setGeometry(QtCore.QRect(220, 50, 60, 60))
         self.pocket11.setObjectName("pocket11")
         self.pocket11.setFont(bt_font)
-        self.pocket11.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
-
+        self.pocket11.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.pocket12 = QtWidgets.QPushButton(self.frame)
         self.pocket12.setGeometry(QtCore.QRect(120, 50, 60, 60))
         self.pocket12.setObjectName("pocket12")
         self.pocket12.setFont(bt_font)
-        self.pocket12.setStyleSheet("color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white" )
-
-
+        self.pocket12.setStyleSheet(
+            "color: white;""background-color: grey;""border-radius : 30px; border : 2px solid white")
 
         self.mankla1 = QtWidgets.QPushButton(self.frame)
         self.mankla1.setGeometry(QtCore.QRect(640, 100, 75, 260))
         self.mankla1.setFont(bt_font)
         self.mankla1.setObjectName("mankla1")
-        self.mankla1.setStyleSheet("color: white;""background-color: grey;" "selection-color: red;""selection-background-color: blue;")
+        self.mankla1.setStyleSheet(
+            "color: white;""background-color: grey;" "selection-color: red;""selection-background-color: blue;")
 
-
-
-         
         self.mankla2 = QtWidgets.QPushButton(self.frame)
         self.mankla2.setGeometry(QtCore.QRect(20, 100, 75, 260))
         self.mankla2.setFont(bt_font)
         self.mankla2.setObjectName("mankla2")
-        self.mankla2.setStyleSheet("color: white;""background-color: grey;" "selection-color: red;""selection-background-color: blue;")
-
-
+        self.mankla2.setStyleSheet(
+            "color: white;""background-color: grey;" "selection-color: red;""selection-background-color: blue;")
 
         ##############  start / save /load  ##########
 
-         
         self.start_bt = QtWidgets.QPushButton(self.bts_frame)
         self.start_bt.setGeometry(QtCore.QRect(220, 40, 121, 31))
         self.start_bt.setFont(lb_font)
         self.start_bt.setObjectName("start_bt")
-        self.start_bt.setStyleSheet("background-color: grey;" )
+        self.start_bt.setStyleSheet("background-color: grey;")
 
-        
         self.load_bt = QtWidgets.QPushButton(self.bts_frame)
         self.load_bt.setGeometry(QtCore.QRect(380, 40, 121, 31))
         self.load_bt.setFont(lb_font)
         self.load_bt.setObjectName("load_bt")
-        self.load_bt.setStyleSheet("background-color: grey;" )
+        self.load_bt.setStyleSheet("background-color: grey;")
 
         self.save_bt = QtWidgets.QPushButton(self.bts_frame)
         self.save_bt.setGeometry(QtCore.QRect(540, 40, 121, 31))
         self.save_bt.setFont(lb_font)
         self.save_bt.setObjectName("save_bt")
-        self.save_bt.setStyleSheet("background-color: grey;" )
-        
-        
+        self.save_bt.setStyleSheet("background-color: grey;")
+
         ######################   labels  #########################
         self.player1_lb = QtWidgets.QLabel(self.frame)
         self.player1_lb.setGeometry(QtCore.QRect(320, 10, 160, 20))
@@ -198,14 +176,9 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.stealing_lb.setGeometry(QtCore.QRect(30, 40, 111, 31))
         self.stealing_lb.setFont(lb_font)
         self.stealing_lb.setObjectName("stealing_lb")
-       
-
-
 
         ######################   fonts  #########################
 
-        
-       
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 867, 21))
@@ -233,7 +206,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.save_bt.clicked.connect(self.save)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
         ################functions ###################
 
     def pkt1(self):
@@ -246,8 +218,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def pkt3(self):
         pass
+
     def pkt4(self):
         pass
+
     def pkt5(self):
         pass
 
@@ -256,8 +230,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def pkt7(self):
         pass
+
     def pkt8(self):
         pass
+
     def pkt9(self):
         pass
 
@@ -266,8 +242,10 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def pkt11(self):
         pass
+
     def pkt12(self):
         pass
+
     def save(self):
         # self.save_state_file(self.get_game_state())
         self.get_game_state()
@@ -275,8 +253,8 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         pass
 
     def load(self):
-        
-        state=self.load_state_file()
+
+        state = self.load_state_file()
 
         self.set_game_state(state)
 
@@ -286,27 +264,24 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
     def start(self):
         pass
 
-
     ######################### help functions #########################
     def is_stealing(self):
         # 1 if stealing   / 0  not stealing 
-        return self.stealing_lb.isChecked() 
+        return self.stealing_lb.isChecked()
 
         # import pathlib
         # pathlib.Path().absolute()
 
-    def save_state_file(self,curent_state):
+    def save_state_file(self, curent_state):
         import json
-       
-        # file_state= open("save.json",'w+')
-        file_state= open(r"E:\4th computer\2nd_term\AI\project\save.json",'w+')
 
-        prev_state=file_state.readlines()
-        ps=json.loads(prev_state[0])
+        # file_state= open("save.json",'w+')
+        file_state = open(r"E:\4th computer\2nd_term\AI\project\save.json", 'w+')
+
+        prev_state = file_state.readlines()
+        ps = json.loads(prev_state[0])
         file_state.writelines(curent_state)
         file_state.close()
-
-        
 
     def load_state_file(self):
         import json
@@ -314,92 +289,86 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         # cwd = str.format(os.getcwd())
 
         # file_state= open( cwd + "\save.josn",'r')
-        file_state= open(r"E:\4th computer\2nd_term\AI\project\save.json",'r')
+        file_state = open(r"E:\4th computer\2nd_term\AI\project\save.json", 'r')
 
-        state=file_state.readlines()
+        state = file_state.readlines()
         file_state.close()
 
-        
         # list to dictionery
         return json.loads(state[0])
         # return state
 
+    def set_game_state(self, game_state):
 
+        self.pocket1.setText(str(game_state['mancala_state'][0]))
+        self.pocket2.setText(str(game_state['mancala_state'][1]))
+        self.pocket3.setText(str(game_state['mancala_state'][2]))
+        self.pocket4.setText(str(game_state['mancala_state'][3]))
+        self.pocket5.setText(str(game_state['mancala_state'][4]))
+        self.pocket6.setText(str(game_state['mancala_state'][5]))
+        self.mankla1.setText(str(game_state['mancala_state'][6]))
 
-    def set_game_state(self , game_state):
+        self.pocket7.setText(str(game_state['mancala_state'][7]))
+        self.pocket8.setText(str(game_state['mancala_state'][8]))
+        self.pocket9.setText(str(game_state['mancala_state'][9]))
+        self.pocket10.setText(str(game_state['mancala_state'][10]))
+        self.pocket11.setText(str(game_state['mancala_state'][11]))
+        self.pocket12.setText(str(game_state['mancala_state'][12]))
+        self.mankla2.setText(str(game_state['mancala_state'][13]))
 
-        self.pocket1.setText(str(game_state['mankala_state'][0]))
-        self.pocket2.setText(str(game_state['mankala_state'][1]))
-        self.pocket3.setText(str(game_state['mankala_state'][2]))
-        self.pocket4.setText(str(game_state['mankala_state'][3]))
-        self.pocket5.setText(str(game_state['mankala_state'][4]))
-        self.pocket6.setText(str(game_state['mankala_state'][5]))
-        self.mankla1.setText(str(game_state['mankala_state'][6]))
-
-        self.pocket7.setText(str(game_state['mankala_state'][7]))
-        self.pocket8.setText(str(game_state['mankala_state'][8]))
-        self.pocket9.setText(str(game_state['mankala_state'][9]))
-        self.pocket10.setText(str(game_state['mankala_state'][10]))
-        self.pocket11.setText(str(game_state['mankala_state'][11]))
-        self.pocket12.setText(str(game_state['mankala_state'][12]))
-        self.mankla2.setText(str(game_state['mankala_state'][13]))
-
-        if game_state['stealing']is True:
+        if game_state['stealing'] is True:
             # self.stealing_lb.setChecked(True)
             self.stealing_lb.nextCheckState()
             # self.stealing_lb.setCheckable(False)
-        else :
+        else:
             # self.stealing_lb.setChecked(False)
             self.stealing_lb.nextCheckState()
-        
 
         # for i in range(6):
-        #     self.pocket+(i+1).setText(game_state['mankala_state'][i])
+        #     self.pocket+(i+1).setText(game_state['mancala_state'][i])
 
         # for i in range(6):
-        #     self.pocket+(i+7).setText(game_state['mankala_state'][i+7])
-
-        
+        #     self.pocket+(i+7).setText(game_state['mancala_state'][i+7])
 
     def get_game_state(self):
 
         import json
-       
-        # file_state= open("save.json",'w+')
-        file_state= open(r"E:\4th computer\2nd_term\AI\project\save.json",'r')
 
-        prev_state=file_state.read()
+        # file_state= open("save.json",'w+')
+        file_state = open(r"E:\4th computer\2nd_term\AI\project\save.json", 'r')
+
+        prev_state = file_state.read()
         print(prev_state)
 
         # list to dictionery
-        ps=json.loads(prev_state)
+        ps = json.loads(prev_state)
         print(ps)
-        
-        ps['mankala_state'].clear()
-        print(ps['mankala_state'])
 
-        ps['mankala_state'].append(self.pocket1.text())
-        ps['mankala_state'].append(self.pocket2.text())
-        ps['mankala_state'].append(self.pocket3.text())
-        ps['mankala_state'].append(self.pocket4.text())
-        ps['mankala_state'].append(self.pocket5.text())
-        ps['mankala_state'].append(self.pocket6.text())
-        ps['mankala_state'].append(self.mankla1.text())
-        ps['mankala_state'].append(self.pocket7.text())
-        ps['mankala_state'].append(self.pocket8.text())
-        ps['mankala_state'].append(self.pocket9.text())
-        ps['mankala_state'].append(self.pocket10.text())
-        ps['mankala_state'].append(self.pocket11.text())
-        ps['mankala_state'].append(self.pocket12.text())
-        ps['mankala_state'].append(self.mankla2.text())
+        ps['mancala_state'].clear()
+        print(ps['mancala_state'])
+
+        ps['mancala_state'].append(self.pocket1.text())
+        ps['mancala_state'].append(self.pocket2.text())
+        ps['mancala_state'].append(self.pocket3.text())
+        ps['mancala_state'].append(self.pocket4.text())
+        ps['mancala_state'].append(self.pocket5.text())
+        ps['mancala_state'].append(self.pocket6.text())
+        ps['mancala_state'].append(self.mankla1.text())
+        ps['mancala_state'].append(self.pocket7.text())
+        ps['mancala_state'].append(self.pocket8.text())
+        ps['mancala_state'].append(self.pocket9.text())
+        ps['mancala_state'].append(self.pocket10.text())
+        ps['mancala_state'].append(self.pocket11.text())
+        ps['mancala_state'].append(self.pocket12.text())
+        ps['mancala_state'].append(self.mankla2.text())
 
         print(ps)
-        ps['stealing']=self.is_stealing()
-        ps2=json.dumps(ps).replace("'", '"')
+        ps['stealing'] = self.is_stealing()
+        ps2 = json.dumps(ps).replace("'", '"')
 
         file_state.close()
 
-        file_state= open(r"E:\4th computer\2nd_term\AI\project\save.json",'w')
+        file_state = open(r"E:\4th computer\2nd_term\AI\project\save.json", 'w')
 
         # convert dic to list 
         # list=[]
@@ -409,16 +378,11 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         #     temp = [key,value]
         #     list.append(temp)
 
-        
         print(ps2)
         file_state.writelines(ps2)
         file_state.close()
 
         # return mankala_state
-
-
-        
-        
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -447,6 +411,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
